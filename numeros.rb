3614e8615757e50d8e1c0d5407fe803754105f81
MarceloACH <cas21014@byui.edu>
2022-05-26 18:48:48 -0400

    Agregando Integer y Float

---
+++ numeros.rb
@@ -0,0 +1,25 @@
+# Integer
+
+numero1 = 20
+numero2 = 30
+
+sumatoria = numero1 + numero2
+
+puts sumatoria
+puts numero1.class
+puts numero2.class
+puts sumatoria.class
+
+puts ""
+
+# Float
+
+decimales1 = 2.15
+decimales2 = 7.56
+
+producto = decimales1 * decimales2
+
+puts producto
+puts decimales1.class
+puts decimales2.class
+puts producto.class
