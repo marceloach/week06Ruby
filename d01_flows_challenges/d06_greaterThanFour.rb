20746c04f52f6dff2167c0837ea78aec88bf7b2e
MarceloACH <cas21014@byui.edu>
2022-06-06 11:36:29 -0400

    Actualización instrucciones

--- d01_flows_challenges/d06_greaterThanFour.rb
+++ d01_flows_challenges/d06_greaterThanFour.rb
@@ -1,7 +1,22 @@
 =begin 
-	Desafio:
-	Crear un programa donde el usuario deba ingresar cuatro números.
-	El programa debe imprimir el mayor de los cuatro números.
+	DESAFIO: EL MAYOR DE CUATRO NÚMEROS
+
+  INSTRUCCIONES
+	Se pide crear el programa "d06_greaterThanFour.rb":
+		Este script debe tomar los 4 argumentos y determinar cuál es el mayor.
+		Si el cuarto argumento es omitido sólo se debe calcular el mayor de 3.
+
+	¿Qué será ARGV[3] si se entregan sólo tres argumentos?
+
+	Uso:
+	ruby mayor_de_4.rb 10 5 3 12
+	12
+	
+	ruby mayor_de_4.rb 12 12 12 9
+	12
+
+	ruby mayor_de_4.rb -21 9 39 0
+	39
 =end
 
 # Almacenar los inputs del usuario en variables
