6ba4e41b1beafb202f896fa592ec5d50ef673b12
MarceloACH <cas21014@byui.edu>
2022-06-06 11:23:34 -0400

    Agrupando los desafios de piedra, papel y tijera

--- d01_flows_challenges/dPiedraPapelTijera/d04_juego.rb
+++ d01_flows_challenges/dPiedraPapelTijera/d04_juego.rb
@@ -1,10 +1,35 @@
 =begin 
-    Desafio:
-    Crear un programa donde el usuario pasará como argumento piedra, papel o tijera.
-    El programa escogerá una opción al azar.
-    IMPORTANT:
-    Desarrollar el diagrama de flujo.
+  DESAFIO: PIEDRA, PAPEL O TIJERA
+
+  INSTRUCCIONES
+    Se pide crear el programa "d04_juego.rb", donde el usuario pasará como argumento piedra, papel o tijera, y el programa escogerá una opción al azar.
+		
+		Crear el diagrama de flujo antes del programa.
+
+		Para que el computador pueda jugar escoger un número al azar entre 0 y 2, si es 0 entonces es piedra, si es 1 entonces papel y 2 tijera.
+
+		Para esto investigue en la documentación el método rand de la clase Random.
+
+		Uso:
+		ruby d04_juego.rb piedra
+		Computador juega tijera.
+		Ganaste.
+
+		ruby d04_juego.rb tijera
+		Computador juega tijera.
+		Empataste.
+
+		ruby d04_juego.rb tijera
+		Computador juega piedra
+		Perdiste.
+
+		En caso que el argumento sea distinto a piedra, papel o tijera el programa debe mostrar las opciones que se pueden jugar:
+
+		ruby d04_juego.rb papelon
+		Argumento invalido: Debe ser piedra, papel o tijera.
 =end
+
+### Diagra de flujo: d04_juegoFlowchart.png ###
 
 # Almacenar la opción del usuario en una variable
 jugador = ARGV[0]
