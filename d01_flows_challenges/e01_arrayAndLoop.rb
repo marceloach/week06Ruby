4344f97aad834ebb3fea153068e780be5eef2201
MarceloACH <cas21014@byui.edu>
2022-06-12 18:25:03 -0400

    Se actualiza comentario

--- d01_flows_challenges/e01_arrayAndLoop.rb
+++ d01_flows_challenges/e01_arrayAndLoop.rb
@@ -1,30 +1,30 @@
 # Solución alternativa para el desafio evaluado d06_greaterThanFour.rb
 
-numbersList = ARGV
+# numbersList = ARGV
 
-graeterThan = ARGV[0].to_i
+# graeterThan = ARGV[0].to_i
 
-for number in numbersList do
-    if number.to_i >= graeterThan
-        graeterThan = number.to_i
-    end
-end
-
-puts graeterThan
-
-# # Almacenar los inputs del usuario en variables
-# number1 = ARGV[0].to_i
-# number2 = ARGV[1].to_i
-# number3 = ARGV[2].to_i
-# number4 = ARGV[3].to_i
-
-# # Si escoge el número mayor de todos
-# if number1 > number2 && number1 > number3 && number1 > number4
-# 	puts number1
-# elsif number2 > number3 && number2 > number4
-# 	puts number2
-# elsif number3 > number4
-# 	puts number3
-# else
-# 	puts number4
+# for number in numbersList do
+#     if number.to_i >= graeterThan
+#         graeterThan = number.to_i
+#     end
 # end
+
+# puts graeterThan
+
+# Almacenar los inputs del usuario en variables
+number1 = ARGV[0].to_i
+number2 = ARGV[1].to_i
+number3 = ARGV[2].to_i
+number4 = ARGV[3].to_i
+
+# Si escoge el número mayor de todos
+if number1 > number2 && number1 > number3 && number1 > number4
+	puts number1
+elsif number2 > number3 && number2 > number4
+	puts number2
+elsif number3 > number4
+	puts number3
+else
+	puts number4
+end
