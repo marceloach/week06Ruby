11c3fa84101de5a267e8e8b3bfb137c08de99e3f
MarceloACH <cas21014@byui.edu>
2022-06-12 18:22:57 -0400

    Métodos finalizados

--- d02_loopsAndMethods_challenges/dPatronesCiclosAnidados/d15_patterns.rb
+++ d02_loopsAndMethods_challenges/dPatronesCiclosAnidados/d15_patterns.rb
@@ -196,7 +196,6 @@ def numero_cero(n)
   n.times do
     print "*"
   end
-  puts ""
 end
 
 def navidad(n)
@@ -206,26 +205,6 @@ def navidad(n)
     print "* " * i
     puts ""
   end
-
-  # (n/2+2).times do |i| # Filas
-  #   reverse = ((n/2)+2)
-  #   (n+2).times do |j| # Columnas
-  #     # print i
-  #     # print j
-
-  #     reverse -= 1
-  #     # print reverse
-  #     if i == reverse
-  #       print "*"
-  #     elsif i == (reverse * (-1))
-  #       print "*"
-  #     else
-  #       print " "
-  #     end
-
-  #   end #Cierre ciclo anidado
-  #   puts ""
-  # end #Cierre ciclo padre
 
   # Sección del medio
   (n-3).times do
@@ -237,63 +216,26 @@ def navidad(n)
     print "\n"
   end
 
-  # (n/2).times do
-  #   (n/2+2).times do
-  #     print " "
-  #   end
-  #   print "*"
-  #   puts ""
-  # end 
-  
   # Sección de abajo
   print " "
   (n-2).times do
     print " " * (n-(n-1))
     print "*"
   end
-
-  # (n/2+1).times do
-  #   print " "
-  #   (n-(n-1)).times do
-  #     print "*"
-  #   end
-  # end
-  # puts ""
 end
 
 # Llamado de métodos
 
 input = ARGV[0].to_i
 
-# letra_o(input)
-# puts ""
-# letra_i(input)
-# puts ""
-# letra_z(input)
-# puts ""
-# letra_x(input)
-# puts ""
-# numero_cero(input)
-# puts ""
-# navidad(input)
-
-def letra_x(num)
-  num.times do |i|
-      num.times do |j|
-        # print i
-        # print j
-        print "i=#{i}; j=#{j}; num-(i+1)=#{num-(i+1)} =>"
-        print " "
-        if j == num - (i + 1) # j=0 num=5 i=0 (0 == 5-0-1)
-            print "B "
-        elsif j == i # j=0 i=0
-            print "A "
-        else
-            print "  "
-        end
-      end
-      print "\n"
-  end
-end
-
+letra_o(input)
+puts ""
+letra_i(input)
+puts ""
+letra_z(input)
+puts ""
 letra_x(input)
+puts ""
+numero_cero(input)
+puts ""
+navidad(input)
