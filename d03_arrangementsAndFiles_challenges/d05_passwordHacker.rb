115517465ff6947b673deca1fe308d5eca47b20b
MarceloACH <cas21014@byui.edu>
2022-06-14 14:27:58 -0400

    Se agregadesafio opcional 'Hackeando un password'

---
+++ d03_arrangementsAndFiles_challenges/d05_passwordHacker.rb
@@ -0,0 +1,15 @@
+=begin 
+  DESAFIO: HACKEANDO UN PASSWORD CON UN DICCIONARIO
+
+  INSTRUCCIONES:
+  Crear un programa donde ocuparemos un archivo que tiene un diccionario de posibles claves con una palabra por línea. 
+  
+  Ocuparemos este archivo para ir probando combinaciones hasta encontrar una palabra clave.
+
+  Además a cada palabra del diccionario le agregaremos como postfijo 1, 12 y 123. 
+  
+  De esta forma si dentro del diccionario está la palabra password probaremos con: password, password1, password12 y password123 (no existen otras variantes, solo esas tres).
+
+  Uso:
+  ruby d05_passwordHacker.rb nombreArchivoDiccionario palabraHackear
+=end
